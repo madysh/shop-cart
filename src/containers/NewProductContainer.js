31ccc6581a2f1ee862192934480a653b5716bebf
Yauheni Shydlouski <yauheni.shydlouski@itech-art.com>
2017-11-15 17:59:16 +0300

    fixed code-style for NewProductContainer

--- src/containers/NewProductContainer.js
+++ src/containers/NewProductContainer.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
-const NewProductContainer = () => (
-  <div className="new-product col-6">
-  </div>
-)
+class NewProductContainer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      isValid: false
+    }
+  }
 
-export default NewProductContainer
+  render(){
+    return (
+      <div className="new-product col-6">
+        <div className='container-title'>Add product to your cart list</div>
+        <form className="new-product-form">
+          <input type="text" id="new-product-name" className="form-control" placeholder="Product name"/>
+          <input
+            type="number"
+            min="1"
+            id="new-product-price"
+            className="form-control"
+            placeholder="Product price"
+          />
+          <button
+            type="submit"
+            className={"btn " + (this.state.isValid ? '' : 'disabled')}
+            disabled={!this.state.isValid}
+          >
+            Add to list
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewProductContainer;
